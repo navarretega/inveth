b0d9bdb79dcf650a8b17ebbfed285a11feeee1c0
Alex Navarrete <anavarrete.ga@gmail.com>
2020-08-08 21:49:48 -0500

    Pending Files -> Contracts - Token|TokenSale/Crowdsale|Kyc

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,13 @@
 const CustomToken = artifacts.require("CustomToken");
+const CustomTokenSale = artifacts.require("CustomTokenSale");
+const KycContract = artifacts.require("KycContract");
 
 module.exports = async function (deployer) {
-  await deployer.deploy(CustomToken, 10000);
+  const addr = await web3.eth.getAccounts();
+  const initialSupply = 10000;
+  await deployer.deploy(CustomToken, initialSupply);
+  await deployer.deploy(KycContract);
+  await deployer.deploy(CustomTokenSale, 1, addr[0], CustomToken.address, KycContract.address);
+  const customTokenInstance = await CustomToken.deployed();
+  await customTokenInstance.transfer(CustomTokenSale.address, initialSupply);
 };
